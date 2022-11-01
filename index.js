7e1d7b0469b86af08f60f79f48fe99efd0494375
5011 <domcai1993>
2022-11-01 17:30:23 +0800

    add

---
+++ index.js
@@ -0,0 +1,196 @@
+// import { WechatyBuilder } from 'wechaty'
+const { WechatyBuilder } = require('wechaty'); // 微信包
+const schedule = require('./schedule/index'); // 定时任务
+const config = require('./config/index');  // 存放公共变量和superagent的配置
+const untils = require('./utils/index'); // 抽取的共用方法
+const superagent = require('./superagent/index'); // 机器人对话
+// 延时函数，防止检测出类似机器人行为操作
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+let Nofriend = ''
+
+// 微信登入二维码生成
+function onScan(qrcode, status) {
+  require('qrcode-terminal').generate(qrcode); // 在console端显示二维码
+  const qrcodeImageUrl = [
+    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    encodeURIComponent(qrcode),
+  ].join('');
+
+  console.log(qrcodeImageUrl);
+}
+
+// 登录成功回调
+async function onLogin(user) {
+  console.log(`贴心小助理${user}登录了`);
+  const date = new Date()
+  console.log(`当前容器时间:${date}`);
+  if (config.AUTOREPLY) {
+    console.log(`已开启机器人自动聊天模式`);
+  }
+
+  // 登陆后创建定时任务
+  // await initDay();
+}
+
+// 退出登入回调
+function onLogout(user) {
+  console.log(`小助手${user} 已经退出`);
+}
+async function ready() {
+  bot.Contact.findAll().then(async res => {
+    // 拿到所有微信联系人
+    const data = res.filter(item => item.payload.type === 1);
+    // 联系人性别： item.gender()(1:男 2:女 0:隐藏了性别)
+    // 城市： item.city()(获取城市地址)
+    // 获取用户的备注名字 item.alias() : Promise
+    // 获取联系人的类型 这里区分是个人好友还是公众号之类的(type=1为个人)
+    // console.log('data', data)
+    data.map(async item => {
+      // const id = item.id;
+      // const contact = await bot.Contact.load('@5f61ea3508f1191dac8e6514e6d2b80b')
+      // const msg = await contact.say('hello（رً ॣ ॣ ॣ ॣ）')
+      // console.log()
+      // 给每个好友发送消息
+      const { id } = item;
+      const contact = await bot.Contact.load(id)
+      await contact.say('hello（رً ॣ ॣ ॣ ॣ）')
+    })
+    // 把不是好友的信息 发给自己
+    setTimeout(async () => {
+      const { id } = bot.currentUser;
+      console.log('item', id)
+      const contact = await bot.Contact.load(id)
+      await contact.say(Nofriend)
+      Nofriend = '';
+    }, 1000)
+
+
+    // console.log(bot.Contact.city('@644ac59529375e58b4a4f5ef92abc36d5f9ffb9fcffdae18a7a00cb72264b104'))
+  })
+
+  // const contact = await bot.Contact.load('@1f7e435314d58fb4cad5dd91100d7414c5908cfc948166dca50072927130bda8')
+  // const msg = await contact.say(' ')
+  // const contact1 = await bot.Contact.load('@e69ceb0529fe449b72fa155875fbd38e')
+  // const msg1 = await contact1.say('hello（رً ॣ ॣ ॣ ॣ）')
+  // console.log('contact', msg, msg1)
+
+
+}
+function currentUser(data, e, v) {
+  console.log('currentUser', data, e,v)
+}
+function logonoff(data, e, v) {
+  console.log('logonoff', data, e, v)
+}
+// 监听对话
+async function onMessage(msg) {
+  const contact = msg.talker(); // 发消息人
+  const content = msg.text().trim(); // 消息内容
+  const room = msg.room(); // 是否是群消息
+  const aliasName = await contact.alias() || await contact.name(); // 发消息人备注
+  const isText = msg.type() === bot.Message.Type.Text;
+  if (msg.self()) {
+    return;
+  }
+  if (room && isText) {
+    // 如果是群消息 目前只处理文字消息
+    const topic = await room.topic();
+    console.log(`群名: ${topic} 发消息人: ${await contact.name()} 内容: ${content}`);
+  } else if (isText) {
+    // 如果非群消息 目前只处理文字消息
+    console.log(`发消息人: ${aliasName},消息内容: ${content}`, msg, contact);
+    // console.log('test', contact, content, room, contact.alias());
+    const { payload: { type, } } = msg; // 好友的类型
+    const { payload: { alias, name } } = contact; // 好友别名和名称
+    if (type === 7 || content.indexOf('请先发送朋友验证请求') > -1) {
+      Nofriend += `好友名称:${name},别名:${alias}<br/>`
+    }
+    if (content.substr(0, 1) == '?' || content.substr(0, 1) == '？') {
+      let contactContent = content.replace('?', '').replace('？', '');
+      if (contactContent) {
+        let res = await superagent.getRubbishType(contactContent);
+        await delay(2000);
+        await contact.say(res);
+      }
+    } else if (config.AUTOREPLY && config.AUTOREPLYPERSON.indexOf(alias) > -1) {
+      // 如果开启自动聊天且已经指定了智能聊天的对象才开启机器人聊天\
+      if (content) {
+        let reply;
+        if (config.DEFAULTBOT == '0') {
+          // 天行聊天机器人逻辑
+          reply = await superagent.getReply(content);
+          console.log('天行机器人回复：', reply);
+        } else if (config.DEFAULTBOT == '1') {
+          // 图灵聊天机器人
+          reply = await superagent.getTuLingReply(content);
+          console.log('图灵机器人回复：', reply);
+        } else if (config.DEFAULTBOT == '2') {
+          // 天行对接的图灵聊
+          reply = await superagent.getTXTLReply(content);
+          console.log('天行对接的图灵机器人回复：', reply);
+        }
+        try {
+          await delay(2000);
+          await contact.say(reply);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    }
+  }
+}
+
+// 创建微信每日说定时任务
+async function initDay() {
+  console.log(`已经设定每日说任务`);
+
+  schedule.setSchedule(config.SENDDATE, async () => {
+    console.log('你的贴心小助理开始工作啦！');
+    let logMsg;
+    let contact =
+      (await bot.Contact.find({ name: config.NICKNAME })) ||
+      (await bot.Contact.find({ alias: config.NAME })); // 获取你要发送的联系人
+    let one = await superagent.getOne(); //获取每日一句
+    let weather = await superagent.getTXweather(); //获取天气信息
+    let today = await untils.formatDate(new Date()); //获取今天的日期
+    let memorialDay = untils.getDay(config.MEMORIAL_DAY); //获取纪念日天数
+    let sweetWord = await superagent.getSweetWord(); // 获取土味情话
+
+    // 你可以修改下面的 str 来自定义每日说的内容和格式
+    // PS: 如果需要插入 emoji(表情), 可访问 "https://getemoji.com/" 复制插入
+    let str = `${today}\n我们在一起的第${memorialDay}天\n\n元气满满的一天开始啦,要开心噢^_^\n\n今日天气\n${weather.weatherTips}\n${weather.todayWeather}\n每日一句:\n${one}\n\n每日土味情话：\n${sweetWord}\n\n————————最爱你的我`;
+    console.log('str', str)
+    try {
+      logMsg = str;
+      await delay(2000);
+      await contact.say(str); // 发送消息
+    } catch (e) {
+      logMsg = e.message;
+    }
+    console.log(logMsg);
+  });
+}
+
+const bot = WechatyBuilder.build({
+  // name: 'WechatEveryDay',
+  name: 'wechat-bot',
+  puppet: 'wechaty-puppet-wechat', // 如果有token，记得更换对应的puppet
+  puppetOptions: {
+    uos: true
+  }
+})
+
+bot.on('scan', onScan);
+bot.on('login', onLogin);
+bot.on('ready', ready)
+bot.on('currentUser', currentUser)
+bot.on('logout', onLogout)
+bot.on('logonoff', logonoff)
+bot.on('message', onMessage);
+
+bot
+  .start()
+  .then((data) => {
+    console.log('开始登陆微信')
+  })
+  .catch((e) => console.error(e));
